55f8a3f4a56c9b051526140b35ce692c04bae9fc
leejunhwi <kajundrama@gmail.com>
2020-02-21 00:44:57 +0900

    Demo Streaming Service Upload

--- demo-streaming-service/src/main/java/me/kajundrama/demostreamingservice/StreamingController.java
+++ demo-streaming-service/src/main/java/me/kajundrama/demostreamingservice/StreamingController.java
@@ -5,11 +5,14 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.file.Files;
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
@@ -18,6 +21,8 @@ import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBo
 @RequestMapping("/stream")
 public class StreamingController {
 
+  @Autowired
+  ServletContext context;
 
   @GetMapping("/")
   public String getTestPage() {
@@ -25,7 +30,7 @@ public class StreamingController {
   }
 
   @GetMapping("/download")
-  public StreamingResponseBody stream(HttpServletRequest req, @RequestParam("fileName") String fileName) throws Exception {
+  public StreamingResponseBody download(HttpServletRequest req, @RequestParam("fileName") String fileName) throws Exception {
 //    InputStream is = this.getClass().getResourceAsStream(fileName);
 //    System.out.println("file : " + is);
     File file = new ClassPathResource(fileName).getFile();
@@ -34,6 +39,15 @@ public class StreamingController {
     return os -> {
       readAndWrite(is, os);
     };
+  }
+
+  @GetMapping("/download2")
+  public void download2(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    File file = new ClassPathResource("sample.mp4").getFile();
+    System.out.println("file2 : " + file.getName() + ", " + file.exists());
+    response.setContentType(context.getMimeType(file.getName()));
+    response.setContentLength((int) file.length());
+    Files.copy(file.toPath(), response.getOutputStream());
   }
 
   private void readAndWrite(final InputStream is, OutputStream os) throws IOException {
