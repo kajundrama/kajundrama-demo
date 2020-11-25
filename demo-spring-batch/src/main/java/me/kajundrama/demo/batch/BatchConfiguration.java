da4253845fe1cad1dbddb6faef40802028e57466
leejunhwi <kajundrama@gmail.com>
2020-11-25 21:14:49 +0900

    Branch02에서 커밋한 내용입니다.

--- demo-spring-batch/src/main/java/me/kajundrama/demo/batch/BatchConfiguration.java
+++ demo-spring-batch/src/main/java/me/kajundrama/demo/batch/BatchConfiguration.java
@@ -78,6 +78,10 @@ public class BatchConfiguration {
         .build();
   }
 
+  /**
+   * step01 스텝을 정의합니다.
+   * @return
+   */
   @Bean
   public Step step1() {
     return stepBuilderFactory.get("step1")
