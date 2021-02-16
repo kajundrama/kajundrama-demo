f02c5320de62086cb2e69350a30cc47325c81fde
leejunhwi <kajundrama@gmail.com>
2021-02-17 00:35:14 +0900

    kajundrama-study :: Demo Spring Boot Kafka For SpringBoot 1.5.12.RELEASE                     CMAK Build 버전 업로

---
+++ demo-spring-boot-kafka-1.5.12/src/main/java/me/kajundrama/demo/springboot/kafka1512/KafkaController.java
@@ -0,0 +1,30 @@
+package me.kajundrama.demo.springboot.kafka1512;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/kafka")
+@RequiredArgsConstructor
+@Slf4j
+public class KafkaController {
+
+  @Autowired
+  private CustomProducer producer;
+
+  /**
+   * curl -X GET http://localhost:8081/kafka/publish/helloKafka
+   * @param message
+   */
+  @GetMapping("/publish/{message}")
+  public void producer(@PathVariable String message){
+    log.info(">>> start event publish");
+    producer.send(message);
+  }
+
+}
