b8d6669a4175df0598b82875915bb5a14a3cd385
leejunhwi <kajundrama@gmail.com>
2021-02-17 21:59:41 +0900

    Boot Application에 주석내용 추가

--- demo-spring-boot-kafka-1.5.12/src/main/java/me/kajundrama/demo/springboot/kafka1512/DemoSpringBootKafkaApplication.java
+++ demo-spring-boot-kafka-1.5.12/src/main/java/me/kajundrama/demo/springboot/kafka1512/DemoSpringBootKafkaApplication.java
@@ -3,6 +3,11 @@ package me.kajundrama.demo.springboot.kafka1512;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * <pre>
+ *   Spring Boot 1.5.12.RELEASE 버전에서 Kafka 연계를 위한 예제 Application 입니다.
+ * </pre>
+ */
 @SpringBootApplication
 public class DemoSpringBootKafkaApplication {
 
