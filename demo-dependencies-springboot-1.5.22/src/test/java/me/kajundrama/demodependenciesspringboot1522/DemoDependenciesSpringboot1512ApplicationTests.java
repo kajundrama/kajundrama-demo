8c7dc3e03dcdfc78109174722a8dad8be6984115
leejunhwi <kajundrama@gmail.com>
2021-02-25 00:09:10 +0900

    kajundrama-study :: Demo Spring Boot Mail 예제코드 업로드

--- demo-dependencies-springboot-1.5.22/src/test/java/me/kajundrama/demodependenciesspringboot1522/DemoDependenciesSpringboot1512ApplicationTests.java
+++ demo-dependencies-springboot-1.5.22/src/test/java/me/kajundrama/demodependenciesspringboot1522/DemoDependenciesSpringboot1512ApplicationTests.java
@@ -1,4 +1,4 @@
-package me.kajundrama.demodependenciesspringboot1512;
+package me.kajundrama.demodependenciesspringboot1522;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
