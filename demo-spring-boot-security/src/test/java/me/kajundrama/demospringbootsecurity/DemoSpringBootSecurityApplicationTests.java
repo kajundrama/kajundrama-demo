eccef5388bdbf4ddaf851b31ab99fcfec71d8f60
leejunhwi <kajundrama@gmail.com>
2021-02-25 21:33:56 +0900

    kajundrama-study :: Demo Spring Boot Security 업로드

---
+++ demo-spring-boot-security/src/test/java/me/kajundrama/demospringbootsecurity/DemoSpringBootSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package me.kajundrama.demospringbootsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringBootSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
